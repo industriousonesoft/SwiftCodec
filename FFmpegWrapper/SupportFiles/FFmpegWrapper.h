a7df6c372b39fac0d94413db826ac758bd76e419
WangCat <industriousonesoft@gmail.com>
2020-03-07 16:24:48 +0800

    Remove Lib & Add Framework

---
+++ FFmpegWrapper/SupportFiles/FFmpegWrapper.h
@@ -0,0 +1,19 @@
+//
+//  FFmpegWrapper.h
+//  FFmpegWrapper
+//
+//  Created by caowanping on 2020/3/7.
+//  Copyright © 2020 zenet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FFmpegWrapper.
+FOUNDATION_EXPORT double FFmpegWrapperVersionNumber;
+
+//! Project version string for FFmpegWrapper.
+FOUNDATION_EXPORT const unsigned char FFmpegWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFmpegWrapper/PublicHeader.h>
+
+
