194cbf3a1698e754a7761d033fc69510f37aea7c
industriousonesoft <industriousonesoft@gmail.com>
2022-04-05 10:57:05 +0800

    Updated

--- SwiftCodecDemo-1/EncoderDemo/AudioIODevice/include/ZDLocalAudioManager.h
+++
@@ -1,9 +0,0 @@
-//
-//  ZDLocalAudioDeviceManager.h
-//  ZDAudioManagerDemo
-//
-//  Created by Georgy on 05/01/2017.
-//  Copyright © 2017 YuanDuan. All rights reserved.
-//
-#import <ZDAudioIODevice/ZDLocalAudioDeviceManager.h>
-#define ZDLocalAudioManager ZDLocalAudioDeviceManager
