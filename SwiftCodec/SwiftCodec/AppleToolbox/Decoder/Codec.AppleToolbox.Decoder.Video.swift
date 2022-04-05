9b57bb87d791c61cfabc39d6cbfd45178a42438b
industriousonesoft <industriousonesoft@gmail.com>
2022-04-05 09:56:37 +0800

    Purge redundant fiels

--- SwiftCodec/SwiftCodec/AppleToolbox/Decoder/Codec.AppleToolbox.Decoder.Video.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Codec.AppleToolbox.Decoder.Video.swift
-//  SwiftCodec_FFmpeg
-//
-//  Created by Mark Cao on 2020/7/9.
-//  Copyright © 2020 zenet. All rights reserved.
-//
-
-import Foundation
